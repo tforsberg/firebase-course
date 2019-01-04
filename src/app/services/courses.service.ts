5869d47725f2ca87d2a7946323275b333192a97f
Todd Forsberg <tforsberg@gmail.com>
2019-01-04 09:07:58 -0600

    update

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {AngularFirestore} from '@angular/fire/firestore';
-import {Course} from '../model/course';
-import {Observable,of} from 'rxjs';
-import {first, map} from 'rxjs/operators';
-import {convertSnaps} from './db-utils';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Course } from '../model/course';
+import { Observable, of } from 'rxjs';
+import { first, map } from 'rxjs/operators';
+import { convertSnaps } from './db-utils';
 
 @Injectable({
   providedIn: 'root'
